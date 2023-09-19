ab8195c7e009ffc57752d5688ca685c2df8051d7
Luiz Vinícius <viniciusadr244@gmail.com>
2023-09-19 12:02:16 -0300

    sort collections

--- src/com/luiz/collections/Produto.java
+++ src/com/luiz/collections/Produto.java
@@ -1,16 +1,21 @@
 package com.luiz.collections;
 
+import java.util.Objects;
+
 public class Produto {
-    private static long cont = 1;
-    private long id = 0;
+    private static int cont = 1;
+    private int id = 0;
     private String nome;
     private double precoCompra;
     private double precoVenda;
     private int quantidadeComprada;
-    
-    // Não permitir que sejam cadastrados itens com preços inválidos, inclusive preço devenda menor que o preço de compra
-    // Mostrar resumo dos itens com quantidade baixa no estoque (abaixo de 50 itens).
-    // Mostrar um resumo dos itens e suas quantidades disponíveis ordenado pela descrição ou quantidade disponível, a critério do usuário.
+
+    // Não permitir que sejam cadastrados itens com preços inválidos, inclusive
+    // preço devenda menor que o preço de compra
+    // Mostrar resumo dos itens com quantidade baixa no estoque (abaixo de 50
+    // itens).
+    // Mostrar um resumo dos itens e suas quantidades disponíveis ordenado pela
+    // descrição ou quantidade disponível, a critério do usuário.
     public Produto(String nome, double precoCompra, double precoVenda, int quantidadeComprada) {
         this.id = cont++;
         this.nome = nome;
@@ -19,7 +24,29 @@ public class Produto {
         this.quantidadeComprada = quantidadeComprada;
     }
 
-    public long getId() {
+    public Produto() {
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome);
+    }
+
+    // public boolean equals(Produto outro) {
+    // return this.id == outro.id && this.nome.equals(outro.nome);
+    // }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        Produto other = (Produto) obj;
+        return id == other.id && Objects.equals(nome, other.nome);
+    }
+
+    public int getId() {
         return id;
     }
 
--- src/com/luiz/collections/TesteCollection.java
+++ src/com/luiz/collections/TesteCollection.java
@@ -4,12 +4,18 @@ import java.util.*;
 
 public class TesteCollection {
     public static void main(String[] args) {
-        var p1 = new Produto("dadsas", 23, 25, 100);
-        var p2 = new Produto("dadsas", 23, 25, 100);
-        var p3 = new Produto("dadsas", 23, 25, 100);
+        // var p1 = new Produto("dadsas", 23, 25, 100);
+        // var p3 = new Produto("aa", 27, 30, 90);
+        // var p2 = new Produto("dadsas", 23, 25, 100);
+        
+        
+        Integer[] l2 = new Integer[] {5,56,6};
 
-        System.out.println(p1);
-        System.out.println(p2);
-        System.out.println(p3);
+        List<Integer> l3 = new ArrayList<>(List.of(l2));
+        
+        l2[2] = 9;
+        l3.add(7);
+        System.out.println(Arrays.toString(l2));
+        System.out.println(l3);
     }   
 }
