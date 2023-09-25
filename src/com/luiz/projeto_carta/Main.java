64185104210fef3c89e6028cca1adec7974ff10b
Luiz Vinícius <viniciusadr244@gmail.com>
2023-09-25 18:03:06 -0300

    done

--- src/com/luiz/projeto_carta/Main.java
+++ src/com/luiz/projeto_carta/Main.java
@@ -7,21 +7,20 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         try (var scan = new Entrada()) {
-            // int nJogadores = scan.lerOption("Quantos jogadores? ", 2, 4, "Quantidade inválida de jogadores");
-            // List<Jogador> jogadores = new ArrayList<>(nJogadores);
+            int nJogadores = scan.lerOption("Quantos jogadores? ", 2, 4, "Quantidade inválida (Máximo 4)");
+            List<Jogador> jogadores = new ArrayList<>();
             
-            // for (int i = 0; i < nJogadores; i++) {
-            //     var nome = scan.lerString("Nome do " + (i+1) + "º jogador: ", "Nome inválido");
-            //     jogadores.add(new Jogador(nome));
-            // }
+            for (int i = 0; i < nJogadores; i++) {
+                var nome = scan.lerString("Nome do " + (i+1) + "º jogador (sem acentos): ", "Nome inválido");
+                jogadores.add(new Jogador(nome));
+            }
 
-            // int qtdPartidas = scan.lerOption("Quantas partidas? ", 2, 5, "Quantidade inválida de partidas");
-            int tipoCarta = scan.lerOption("""
-            [ 1 ] Carta Normal
-            [ 2 ] Carta Naipe+
-            [ 3 ] Carta Valor+
-            Sua opção: """ + " ", 1, 3, "Opção de carta inválida");
+            int qtdPartidas = scan.lerOption("\nQuantas partidas? ", 1, 5, "Quantidade inválida (Máximo 5)");
+            int tipoCarta = scan.lerOption("[ 1 ] Carta Normal\n[ 2 ] Carta Naipe+\n[ 3 ] Carta Valor+\nSua opção: ", 1, 3, "Opção de carta inválida");
+            System.out.println();
             
+            var rodada = new Rodada(jogadores, tipoCarta, qtdPartidas);
+            rodada.iniciar();
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
--- src/com/luiz/projeto_carta/Rodada.java
+++ src/com/luiz/projeto_carta/Rodada.java
@@ -1,52 +1,83 @@
 package com.luiz.projeto_carta;
 
-import java.util.ArrayList;
-import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Set;
+import java.util.HashSet;
 
 public class Rodada {
     private List<Jogador> jogadores;
-    private int tipoCarta;
-    private int qtdPartidas;
-    private Set<Carta> cartasPartida;
+    private List<Carta> cartasRodada;
+    private int tipoCarta, qtdPartidas;
 
     public Rodada(List<Jogador> jogadores, int tipoCarta, int qtdPartidas) {
         this.jogadores = jogadores;
         this.tipoCarta = tipoCarta;
+        this.cartasRodada = new ArrayList<>();
         this.qtdPartidas = qtdPartidas;
-        this.cartasPartida = new LinkedHashSet<>(jogadores.size());
     }
 
     public void iniciar() {
         for (int i = 0; i < qtdPartidas; i++) {
             this.geraCartas();
             this.mostraCartas();
-            this.cartasPartida.clear();
+            this.cartasRodada.clear();
         }
+        this.placar();
     }
 
     public void geraCartas() {
-        while (this.cartasPartida.size() < this.jogadores.size()) {
+        Set<Carta> cartasUnicas = new HashSet<>();
+        while (cartasUnicas.size() < this.jogadores.size()) {
             switch (this.tipoCarta) {
-                case 1 -> this.cartasPartida.add(new Carta());
-                case 2 -> this.cartasPartida.add(new CartaNaipe());
-                case 3 -> this.cartasPartida.add(new CartaValor());
-                default -> throw new IllegalArgumentException("Tipo de carta inválido");
+                case 1 -> cartasUnicas.add(new Carta());
+                case 2 -> cartasUnicas.add(new CartaNaipe());
+                case 3 -> cartasUnicas.add(new CartaValor());
             };
         }
+        this.cartasRodada.addAll(cartasUnicas);
+        for (int i = 0; i < jogadores.size(); i++) {
+            this.jogadores.get(i).setCarta(this.cartasRodada.get(i));
+        }
+        cartasUnicas.clear();
     }
 
     public void mostraCartas() {
-        List<Carta> cartasArray = new ArrayList<>(this.cartasPartida);
+        // ordena em ordem decrescente
+        this.jogadores.sort((item1, item2) -> Integer.compare(item2.getCarta().getValor(), item1.getCarta().getValor()));
         System.out.printf("%-10s %-25s %10s%n", "Nome", "Carta", "Pontos");
-        Carta carta;
-        Jogador jogador;
-        for (int i = 0; i < jogadores.size(); i++) {
-            carta = cartasArray.get(i);
-            jogador = jogadores.get(i);
-            System.out.printf("%-10s recebeu um(a) %-17s %d%n", jogador.getNome(), carta, carta.getValor());
+        for (Jogador jogador : this.jogadores) {
+            System.out.printf("%-10s recebeu um(a) %-17s %d%n", jogador, jogador.getCarta(), jogador.getCarta().getValor());
         }
+
+        var vencedor = this.jogadorVencedor();
+        System.out.print("Vencedor da rodada: ");
+        vencedor.forEach(jogador -> System.out.println(jogador + " "));
         System.out.println();
+    }
+
+    public List<Jogador> jogadorVencedor() {
+        List<Jogador> maiorPontuacao = new ArrayList<>();
+        var maiorCarta = this.jogadores.get(0).getCarta().getValor();
+        
+        int pontos = 3;
+        for (Jogador jogador: this.jogadores) {
+            if (jogador.getCarta().getValor() == maiorCarta) {
+                jogador.setPontuacao(jogador.getPontuacao() + pontos);
+                maiorPontuacao.add(jogador);
+            } else {
+                pontos--;
+                if (pontos == 0) pontos = 1;
+                jogador.setPontuacao(jogador.getPontuacao() + pontos);
+                maiorCarta = jogador.getCarta().getValor();
+            }
+        }
+        return maiorPontuacao;
+    }
+
+    public void placar() {
+        System.out.println("----------------- Placar Final -----------------");
+        this.jogadores.sort((item1, item2) -> Integer.compare(item2.getPontuacao(), item1.getPontuacao()));
+        this.jogadores.forEach(jogador -> System.out.printf("%s com %d pontos%n", jogador, jogador.getPontuacao()));
     }
 }
