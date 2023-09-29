f725ac0d268289134a85fb3a938bc3ddc6ca7e95
Luiz Vinícius <viniciusadr244@gmail.com>
2023-09-29 09:48:04 -0300

    add locale

---
+++ src/com/luiz/locale/TestLocale.java
@@ -0,0 +1,17 @@
+package com.luiz.locale;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class TestLocale {
+    public static void main(String[] args) {
+        Locale padrao = Locale.getDefault();
+        System.out.println(padrao);
+
+        int price = 100_000;
+        System.out.println(NumberFormat.getCurrencyInstance(Locale.of("pt", "BR")).format(price));
+        System.out.println(NumberFormat.getCurrencyInstance(Locale.US).format(price));
+        System.out.println(NumberFormat.getCurrencyInstance(Locale.UK).format(price));
+        System.out.println(NumberFormat.getCurrencyInstance(Locale.ITALY).format(price));
+    }   
+}
