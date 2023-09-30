8852e514dd53060e9560c437a6eb9bbf2362dd16
Luiz Vinícius <viniciusadr244@gmail.com>
2023-09-30 09:51:50 -0300

    remove unused imports

--- src/com/luiz/datas/DataTest.java
+++ src/com/luiz/datas/DataTest.java
@@ -1,10 +1,8 @@
 package com.luiz.datas;
 
-import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
