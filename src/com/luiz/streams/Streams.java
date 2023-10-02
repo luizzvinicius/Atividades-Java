d49221fa2ef1d028a010dbd281f1208ca1753f9e
Luiz Vinícius <viniciusadr244@gmail.com>
2023-10-02 11:24:08 -0300

    add streams

---
+++ src/com/luiz/streams/Streams.java
@@ -0,0 +1,44 @@
+package com.luiz.streams;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Streams {
+    public static void main(String[] args) {
+        record Hq(String title, double price) {}
+        List<Hq> revistas = new ArrayList<>(
+                List.of(new Hq("Batman", 5), new Hq("Superman", 5.54), new Hq("Iron man", 6.4)));
+
+        /*
+         * Pipeline de dados com loops implícitos
+         * Operações intermediárias: que devolvem um stream e ainda pode aplicar métodos
+         * distinct(): não processar elementos repetidos (utiliza equals e hashCode)
+         * sorted
+         * skip(n): pula uma quantidade de elementos
+         * limit(n): quantos elementos para processar
+         * filter
+         * map
+         * flatMap: usado com listas aninhadas
+         * 
+         * Operações finais: retornam void e o stream fecha.
+         * count
+         * min e max
+         * all, any e noneMatch
+         * findFirst, findAny
+         * 
+         * collect(Collectors.)
+         * Collectors:
+         * toList, toSet
+         * grupingBy: gera um mapa conforme a regra passada ex.: agrupar clientes por endereço
+         * joining: apenas com strings
+        */
+
+                
+    }
+}
