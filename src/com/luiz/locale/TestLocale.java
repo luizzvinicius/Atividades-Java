359e5b33f1baf90c3d8b663209656bc72be07568
Luiz Vinícius <viniciusadr244@gmail.com>
2023-10-11 07:37:57 -0300

    reafactor

--- src/com/luiz/locale/TestLocale.java
+++ src/com/luiz/locale/TestLocale.java
@@ -7,9 +7,9 @@ public class TestLocale {
     public static void main(String[] args) {
         Locale padrao = Locale.getDefault();
         System.out.println(padrao);
-
+        Locale brasil = Locale.of("pt", "BR");
         int price = 100_000;
-        System.out.println(NumberFormat.getCurrencyInstance(Locale.of("pt", "BR")).format(price));
+        System.out.println(NumberFormat.getCurrencyInstance(brasil).format(price));
         System.out.println(NumberFormat.getCurrencyInstance(Locale.US).format(price));
         System.out.println(NumberFormat.getCurrencyInstance(Locale.UK).format(price));
         System.out.println(NumberFormat.getCurrencyInstance(Locale.ITALY).format(price));
