3d3cb4ed6ffb2337352e05597cae0552f76d55ff
Luiz Vinicius <viniciusadr244@gmail.com>
2024-05-11 12:32:56 -0300

    update import

---
+++ src/com/luiz/threads/sync/calculadora/Sync.java
@@ -0,0 +1,11 @@
+package com.luiz.threads.sync.calculadora;
+
+public class Sync {
+    public static void main(String[] args) {
+        // Importante em certificações
+        int[] array = {1,2,3,4,5};
+
+        new MyThread("#1", array);
+        new MyThread("#2", array);
+    }   
+}
